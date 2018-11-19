fc1f967cee0da040d855ae6ba36ab18a6e7d8b9c
saki4510t <saki4510t@gmail.com>
2018-11-20 04:43:37 +0900

    デバッグログを有効、改行を修正

--- ncsdk/src/main/jni/movidious/serenegiant_ncs_movidius.cpp
+++ ncsdk/src/main/jni/movidious/serenegiant_ncs_movidius.cpp
@@ -2,13 +2,13 @@
 // Created by saki on 2018/11/19.
 //
 
-#if 1	// set 0 is you need debug log, otherwise set 1
+#if 0	// set 0 is you need debug log, otherwise set 1
 	#ifndef LOG_NDEBUG
 		#define	LOG_NDEBUG
 	#endif
 	#undef USE_LOGALL
 #else
-//	#define USE_LOGALL
+	#define USE_LOGALL
 	#undef LOG_NDEBUG
 	#undef NDEBUG
 #endif
@@ -84,6 +84,7 @@ static ID_TYPE nativeCreate(JNIEnv *env, jobject thiz) {
 		= new Movidius(obj);
 	setField_long(env, thiz, "mNativePtr", reinterpret_cast<ID_TYPE>(ncs));
 	LOGD("ncs=%p", ncs);
+
 	RETURN(reinterpret_cast<ID_TYPE>(ncs), ID_TYPE);
 }
 
@@ -92,12 +93,14 @@ static void nativeDestroy(JNIEnv *env, jobject thiz,
 	ID_TYPE id_ncs) {
 
 	ENTER();
+
 	setField_long(env, thiz, "mNativePtr", 0);
 	Movidius *ncs = reinterpret_cast<Movidius *>(id_ncs);
 	if (LIKELY(ncs)) {
 		ncs->release(env);
 		SAFE_DELETE(ncs);
 	}
+
 	EXIT();
 }
 
@@ -106,12 +109,14 @@ static jint nativeConnect(JNIEnv *env, jobject thiz,
 	ID_TYPE id_ncs, jint fd) {
 
 	ENTER();
+
 	int result = JNI_ERR;
 	Movidius *ncs = reinterpret_cast<Movidius *>(id_ncs);
 	if (LIKELY(ncs && (fd > 0))) {
 		fd = dup(fd);	// Java側からcloseされてしまわないようにする
 		result = ncs->connect(fd);
 	}
+
 	RETURN(result, jint);
 }
 
@@ -120,11 +125,13 @@ static jint nativeDisconnect(JNIEnv *env, jobject thiz,
 	ID_TYPE id_pipeline) {
 
 	ENTER();
+
 	int result = JNI_ERR;
 	Movidius *ncs = reinterpret_cast<Movidius *>(id_pipeline);
 	if (LIKELY(ncs)) {
 		result = ncs->disconnect();
 	}
+
 	RETURN(result, jint);
 }
 
@@ -140,14 +147,15 @@ static JNINativeMethod methods[] = {
 };
 
 int register_ncs_movidius(JNIEnv *env) {
-	LOGV("");
+	ENTER();
+
 	// ネイティブメソッドを登録
 	if (registerNativeMethods(env,
 		"com/serenegiant/ncs/Movidius",
 		methods, NUM_ARRAY_ELEMENTS(methods)) < 0) {
-		return -1;
+		RETURN(-1, int);
 	}
 
-	return 0;
+	RETURN(0, int);
 }
 
